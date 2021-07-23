811e117ae4d2650ee0c349b2efa642a02eff539a
erez <erez@razor-labs.com>
2021-07-23 18:23:58 +0300

    # Fixed setup.py # Added tox.ini file # Updated .gitignore

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup, find_namespace_packages, find_packages
+from setuptools import setup, find_packages
 
 setup(
     name='generic-types',
     version='0.0.1',
-    packages=find_packages(),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     url='https://github.com/erezinman/generic-types',
     license='MIT License',
     author='Erez Zinman',
